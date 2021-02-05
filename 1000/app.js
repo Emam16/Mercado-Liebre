58a59e3477ef725e5ac95589372265f7c77d3977
Emam16 <ema150798@gmail.com>
2021-02-05 08:12:58 -0300

    Mercado Liebre

---
+++ 1000/app.js
@@ -0,0 +1,43 @@
+const express = require ("express");
+
+//const path = require ("path");
+
+const app = express ();
+
+//const publicPath =path.resolve(__dirname, "./public")
+
+app.use(express.static("Public"));
+
+//app.use(express.static(publicPath));
+
+//app.get ("/", (req,res)=> {
+//let htmlPath = path.resolve (__dirname, "./views/index.html");
+//res.sendFile(htmlPath)
+//});
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+});
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/404', (req,res)=>{
+    res.send("Error pagina no encontrada", 404);
+});
+
+app.listen (1000, ()=>console.log ("Servidor en puerto 1000"))
+
+//res.sendFile ("./views/index.html"))
+
+//app.get ("/contacto", (req,res)=>res.send ("¡Dejanos tu contacto!"));
+
+//app.get ("/un-array", (req,res)=>res.send ([1,2,3,4]));
+
+//app.get ("/un-objeto", (req,res)=>res.send ({name:"Emanuel"}))
+
---
+++ 4000/app.js
@@ -0,0 +1,21 @@
+const express = require ("express");
+const path = require ("path");
+
+const app = express ();
+
+const publicPath =path.resolve(__dirname, "./public")
+
+app.use(express.static(publicPath));
+
+//const express = require ("express");
+//const path = require ("path");
+
+//const app = express ();
+
+//app.use(express.static("public"));
+
+
+
+app.get ("/" , (req , res) => {res.sendFile(path.resolve ("./Views/home.html"))})
+
+app.listen (4000, ()=>console.log ("servidor en puerto 3000"))
